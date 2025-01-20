3cb97987742963ef921d67c62400a0cd7344d4ca
Christian J Wilkins <wilki385@msu.edu>
2025-01-19 21:10:23 -0500

    Hello world working.

---
+++ src/test/java/com/chris/portfolio2025/Portfolio2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.portfolio2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Portfolio2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
