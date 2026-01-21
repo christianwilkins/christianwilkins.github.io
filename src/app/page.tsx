d3fa86f1a35a10c420a8611bb9b852700fb4a213
Christian J Wilkins <wilki385@msu.edu>
2026-01-21 13:32:04 -0500

    Refine home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold mb-6 font-heading">HOME</h1>
+    <div className="animate-rise-in">
+      <h1 className="text-4xl font-bold mb-6 font-heading">Home</h1>
       <p className="mb-4 text-lg leading-relaxed">Hi, I&apos;m Chris!</p>
       <p className="mb-4 text-lg leading-relaxed">
         I&apos;m a software engineer passionate about building meaningful solutions
-        that make a difference. Currently based in the US (US Citizen), I
-        specialize in full-stack development and love tackling complex problems.
+        that make a difference. Currently based in the United States, and I am a
+        United States citizen, I specialize in full-stack development and love
+        tackling complex problems.
       </p>
       <p className="mb-4 text-lg leading-relaxed">
         Take a look at my{" "}
