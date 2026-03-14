b70f5dc419c00c9cf66e1a77e85f7d413f8fe4c7
Christian Wilkins <chrisjw12345@gmail.com>
2026-03-14 10:15:45 -0400

    add cooking nav item

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -9,6 +9,7 @@ export const primaryNavItems: NavItem[] = [
   { id: "about", href: "/", label: "About" },
   { id: "consulting", href: "/consulting", label: "Consulting" },
   { id: "projects", href: "/projects", label: "Projects" },
+  { id: "cooking", href: "/cooking", label: "Cooking Blog" },
   { id: "contact", href: "/contact", label: "Contact" },
   { id: "lab", href: "/lab", label: "The Lab" },
 ];
