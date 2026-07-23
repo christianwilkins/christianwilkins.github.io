892c561c215d656b7b800c19dde04d308b5979e7
Christian Wilkins <chrisjw12345@gmail.com>
2026-07-23 17:12:53 -0400

    perf: split Three.js into separate chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ["three"],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
